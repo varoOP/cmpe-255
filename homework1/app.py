3390609863cfe78fa88782978f595ce14c11639b
varoOP <46842159+varoOP@users.noreply.github.com>
2024-02-24 21:44:13 -0800

    docs: add source code link

--- homework1/app.py
+++ homework1/app.py
@@ -17,10 +17,12 @@ query_job = client.query(query)
 df = query_job.to_dataframe()
 
 st.title('CMPE 255 Homework 1')
+
 st.markdown("""
 - **Name:** Rohit Vardam
 - **SJSU ID:** 017437433
 - **Original Dataset:** bigquery-public-data.medicare.outpatient_charges_2014
+- **Source Code:** [GitHub Repository](https://github.com/varoOP/cmpe-255/tree/main/homework1)
 """)
 
 st.header('Data Visualization')
