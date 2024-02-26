9409aae0da856031a08f48306fe055c8df9159c2
varoOP <46842159+varoOP@users.noreply.github.com>
2024-02-26 00:40:45 -0800

    fix: use eps images instead of svg

--- homework1/app.py
+++ homework1/app.py
@@ -30,7 +30,7 @@ st.header('Data Visualization')
 st.subheader('Box Plot')
 box_fig = px.box(df, x='provider_state', y='average_total_payments', title="Statewise Average Total Payments")
 st.plotly_chart(box_fig, use_container_width=True)
-box_fig_file = "statewise_average_total_payments.svg"
+box_fig_file = "box.eps"
 if not os.path.exists(box_fig_file):
     box_fig.write_image(box_fig_file) 
 
@@ -38,7 +38,7 @@ st.subheader('Bar Graph')
 grouped_data = df.groupby('apc')['outpatient_services'].sum().reset_index()
 bar_fig = px.bar(grouped_data, x='apc', y='outpatient_services', text_auto=True, title="APC by Number of Outpatient Services")
 st.plotly_chart(bar_fig, use_container_width=True)
-bar_fig_file = "apc_by_outpatient_services.svg"
+bar_fig_file = "bar.eps"
 if not os.path.exists(bar_fig_file):
     bar_fig.write_image(bar_fig_file) 
 
@@ -48,6 +48,6 @@ map_fig = px.scatter_mapbox(df_filtered, lat="latitude", lon="longitude", size="
                             color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=3,
                             mapbox_style="carto-positron", title="Provider Locations and Average Estimated Submitted Charges")
 st.plotly_chart(map_fig, use_container_width=True)
-map_fig_file = "provider_locations_and_charges.svg"
+map_fig_file = "map.eps"
 if not os.path.exists(map_fig_file):
     map_fig.write_image(map_fig_file) 
